12ad7fdd59e3bf607a84d1b91d8bbda7af0bbc56
Eli Lemons <me@elilemons.com>
2023-12-18 18:18:43 -0500

    Survey has its owned points earned now

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -34,6 +34,7 @@ export interface Survey {
   id: string;
   title?: string | null;
   status?: ('started' | 'uncompleted' | 'completed') | null;
+  pointsEarned?: number | null;
   surveyDate: string;
   surveyUser?: (string | null) | User;
   surveyQuestionSets?: (string | QuestionSet)[] | null;
